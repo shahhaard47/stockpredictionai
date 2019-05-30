7d76acc0ac7f7b6d708e546f6c6d3c438217806c
Haard Shah <hks32@njit.edu>
2019-05-30 14:11:46 -0400

    trying to implement stock prediction from shitty documentation

---
+++ stockpred.py
@@ -0,0 +1,160 @@
+"""Reproducing https://github.com/borisbanushev/stockpredictionai"""
+
+# from utils import * # WTF where is this
+
+import time
+import numpy as np
+
+# mxnet stuff 
+# mxnet
+# mxnet
+
+import datetime
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+import math
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error, accuracy_score
+
+# xgboost
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+
+context = mx.cpu(); model_ctx=mx.cpu()
+mx.random.seed(1719)
+
+def parser(x):
+    return datetime.datetime.strptime(x, '%Y-%m-%d')
+
+dataset_ex_df = pd.read_csv("GS.csv", header=0, parse_dates=[0], date_parser=parser)
+
+dataset_ex_df[['Date', 'Close']].head(3)
+
+print('There are {} number of days in the dataset.'.format(dataset_ex_df.shape[0]))
+
+#% lets Visualize the last nine years!
+plt.figure(figsize=(14, 5), dpi=100)
+plt.plot(dataset_ex_df['Date'], dataset_ex_df['Close'], label='Goldman Sachs stock')
+plt.vlines(datetime.date(2016,4, 20), 0, 270, linestyles='--', colors='gray', label='Train/Test data cut-off')
+plt.xlabel('Date')
+plt.ylabel('USD')
+plt.title('Figure 2: Goldman Sachs stock price')
+plt.legend()
+plt.show()
+
+# get training days
+num_training_days = int(dataset_ex_df.shape[0]*.7)
+print('Number of training days: {}. Number of test days: {}.'.format(num_training_days, dataset_ex_df.shape[0]-num_training_days))
+
+# 3.1 Correlate assets (IDFK what data to add :// `article says 72 other assets`)
+
+# 3.2 Technical Indicators
+def get_technical_indicators(dataset):
+    # Create 7 and 21 days Moving Average
+    dataset['ma7'] = dataset['price'].rolling(window=7).mean()
+    dataset['ma21'] = dataset['price'].rolling(window=21).mean()
+    
+    # Create MACD
+    dataset['26ema'] = pd.ewma(dataset['price'], span=26)
+    dataset['12ema'] = pd.ewma(dataset['price'], span=12)
+    dataset['MACD'] = (dataset['12ema']-dataset['26ema'])
+
+    # Create Bollinger Bands
+    dataset['20sd'] = pd.stats.moments.rolling_std(dataset['price'],20)
+    dataset['upper_band'] = dataset['ma21'] + (dataset['20sd']*2)
+    dataset['lower_band'] = dataset['ma21'] - (dataset['20sd']*2)
+    
+    # Create Exponential moving average
+    dataset['ema'] = dataset['price'].ewm(com=0.5).mean()
+    
+    # Create Momentum
+    dataset['momentum'] = dataset['price']-1
+    
+    return dataset
+
+# this doesn't work because the dataset is different
+dataset_TI_df = get_technical_indicators(dataset_ex_df[['Close']])
+
+# Visualize last 400 days of these indicators
+def plot_technical_indicators(dataset, last_days):
+    plt.figure(figsize=(16, 10), dpi=100)
+    shape_0 = dataset.shape[0]
+    xmacd_ = shape_0-last_days
+    
+    dataset = dataset.iloc[-last_days:, :]
+    x_ = range(3, dataset.shape[0])
+    x_ =list(dataset.index)
+    
+    # Plot first subplot
+    plt.subplot(2, 1, 1)
+    plt.plot(dataset['ma7'],label='MA 7', color='g',linestyle='--')
+    plt.plot(dataset['price'],label='Closing Price', color='b')
+    plt.plot(dataset['ma21'],label='MA 21', color='r',linestyle='--')
+    plt.plot(dataset['upper_band'],label='Upper Band', color='c')
+    plt.plot(dataset['lower_band'],label='Lower Band', color='c')
+    plt.fill_between(x_, dataset['lower_band'], dataset['upper_band'], alpha=0.35)
+    plt.title('Technical indicators for Goldman Sachs - last {} days.'.format(last_days))
+    plt.ylabel('USD')
+    plt.legend()
+
+    # Plot second subplot
+    plt.subplot(2, 1, 2)
+    plt.title('MACD')
+    plt.plot(dataset['MACD'],label='MACD', linestyle='-.')
+    plt.hlines(15, xmacd_, shape_0, colors='g', linestyles='--')
+    plt.hlines(-15, xmacd_, shape_0, colors='g', linestyles='--')
+    plt.plot(dataset['log_momentum'],label='Momentum', color='b',linestyle='-')
+
+    plt.legend()
+    plt.show()
+
+plot_technical_indicators(dataset_TI_df, 400)
+
+
+# 3.3 Fundamental Analysis
+
+# BERT
+# !pip install bert
+import bert
+
+# 3.4 Fourier transfroms for trend analysis (denoising data)
+        # Wavelets are another option for denoising
+        # "both give similar results so we use Fourier transforms"
+data_FT = dataset_ex_df[['Date', 'Close']]
+
+close_fft = np.fft.fft(np.asarray(data_FT['Close'].tolist()))
+fft_df = pd.DataFrame({'fft':close_fft})
+fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
+fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+# plot it
+plt.figure(figsize=(14, 7), dpi=100)
+fft_list = np.asarray(fft_df['fft'].tolist())
+for num_ in [3, 6, 9, 100]:
+    fft_list_m10= np.copy(fft_list); fft_list_m10[num_:-num_]=0
+    plt.plot(np.fft.ifft(fft_list_m10), label='Fourier transform with {} components'.format(num_))
+plt.plot(data_FT['Close'],  label='Real')
+plt.xlabel('Days')
+plt.ylabel('USD')
+plt.title('Figure 3: Goldman Sachs (close) stock prices & Fourier transforms')
+plt.legend()
+plt.show()
+
+# WHAT IS THIS?
+from collections import deque
+items = deque(np.asarray(fft_df['absolute'].tolist()))
+items.rotate(int(np.floor(len(fft_df)/2)))
+plt.figure(figsize=(10, 7), dpi=80)
+plt.stem(items)
+plt.title('Figure 4: Components of Fourier transforms')
+plt.show()
+
+# 3.5 ARIMA as a feature
+
+
